cb4877efcb35cac871e368b76fe01714ad541f65
Marcin Kufel <marcinkufel84@gmail.com>
2021-04-20 17:16:30 +0200

    added condition witch check if button is checkec

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ const form8 = document.querySelector(".js-form8");
 const form9 = document.querySelector(".js-form9");
 const form10 = document.querySelector(".js-form10");
 
-
+const reloadButton = document.querySelector(".js-reloadButton");
 
 const answer = document.querySelectorAll(".js-answer");
 
@@ -36,46 +36,63 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
 
 
+    if (answer[0].checked == false && answer[1].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-if (answer[0].value == 1 && answer[0].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    } else if (answer[0].value == 1 && answer[0].checked == 1) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question1.classList.add("question--hidden");
+        question2.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question1.classList.add("question--hidden");
+        question2.classList.remove("question--hidden");
+    }
 
-question1.classList.add("question--hidden");
-question2.classList.remove("question--hidden");
+
 
 })
 
 form2.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[3].value == 1 && answer[3].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[2].checked == false && answer[3].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question2.classList.add("question--hidden");
-question3.classList.remove("question--hidden");
+    } else if (answer[3].value == 1 && answer[3].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question2.classList.add("question--hidden");
+        question3.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question2.classList.add("question--hidden");
+        question3.classList.remove("question--hidden");
+    }
+
+
 
 })
 
 form3.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[5].value == 1 && answer[5].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[4].checked == false && answer[5].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question3.classList.add("question--hidden");
-question4.classList.remove("question--hidden");
+    } else if (answer[5].value == 1 && answer[5].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question3.classList.add("question--hidden");
+        question4.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question3.classList.add("question--hidden");
+        question4.classList.remove("question--hidden");
+    }
+
+
 
 })
 
@@ -83,15 +100,21 @@ question4.classList.remove("question--hidden");
 form4.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[6].value == 1 && answer[6].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[6].checked == false && answer[7].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question4.classList.add("question--hidden");
-question5.classList.remove("question--hidden");
+    } else if (answer[6].value == 1 && answer[6].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question4.classList.add("question--hidden");
+        question5.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question4.classList.add("question--hidden");
+        question5.classList.remove("question--hidden");
+    }
+
+
 
 })
 
@@ -99,89 +122,129 @@ question5.classList.remove("question--hidden");
 form5.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[8].value == 1 && answer[8].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[8].checked == false && answer[9].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question5.classList.add("question--hidden");
-question6.classList.remove("question--hidden");
+    } else if (answer[8].value == 1 && answer[8].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question5.classList.add("question--hidden");
+        question6.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question5.classList.add("question--hidden");
+        question6.classList.remove("question--hidden");
+    }
+
+
 
 })
 
 form6.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[11].value == 1 && answer[11].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[10].checked == false && answer[11].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question6.classList.add("question--hidden");
-question7.classList.remove("question--hidden");
+    } else if (answer[11].value == 1 && answer[11].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question6.classList.add("question--hidden");
+        question7.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question6.classList.add("question--hidden");
+        question7.classList.remove("question--hidden");
+    }
+
+
 
 })
 
 form7.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[12].value == 1 && answer[12].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[12].checked == false && answer[13].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question7.classList.add("question--hidden");
-question8.classList.remove("question--hidden");
+    } else if (answer[12].value == 1 && answer[12].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question7.classList.add("question--hidden");
+        question8.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question7.classList.add("question--hidden");
+        question8.classList.remove("question--hidden");
+    }
+
+
 
 })
 
 form8.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[15].value == 1 && answer[15].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[14].checked == false && answer[15].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question8.classList.add("question--hidden");
-question9.classList.remove("question--hidden");
+    } else if (answer[15].value == 1 && answer[15].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question8.classList.add("question--hidden");
+        question9.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question8.classList.add("question--hidden");
+        question9.classList.remove("question--hidden");
+    }
+
+
 
 })
 
 form9.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[16].value == 1 && answer[16].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your score: ${sum}`;
-}
+    if (answer[16].checked == false && answer[17].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question9.classList.add("question--hidden");
-question10.classList.remove("question--hidden");
+    } else if (answer[16].value == 1 && answer[16].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your score: ${sum}`;
+        question9.classList.add("question--hidden");
+        question10.classList.remove("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your score: ${sum}`;
+        question9.classList.add("question--hidden");
+        question10.classList.remove("question--hidden");
+    }
+
+
 
 })
 
 form10.addEventListener("submit", (event) => {
     event.preventDefault();
 
-if (answer[18].value == 1 && answer[18].checked) {
-    sum = sum + 1;
-    result.innerHTML = `Good answer ! Your final score: ${sum}`;
-} else {
-    result.innerHTML = `Bad answer ! Your final score: ${sum}`;
-}
+    if (answer[18].checked == false && answer[19].checked == false) {
+        result.innerHTML = `Please check any answer`;
 
-question10.classList.add("question--hidden");
+    } else if (answer[18].value == 1 && answer[18].checked) {
+        sum = sum + 1;
+        result.innerHTML = `Good answer ! Your final score: ${sum}`;
+        question10.classList.add("question--hidden");
+    } else {
+        result.innerHTML = `Bad answer ! Your final score: ${sum}`;
+        question10.classList.add("question--hidden");
+    }
+
+
 
 })
 
+reloadButton.addEventListener("click", () => {
+
+    location.reload();
+
+})
